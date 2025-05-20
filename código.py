e6cbd742fa4c708a6ed02bf6911e328109e2a3e3
André Mann (l7 me espera) <andre.mann@escola.pr.gov.br>
2025-05-20 10:26:06 -0300

    Create código.py

---
+++ código.py
@@ -0,0 +1,18 @@
+frutas = ["uva",  "banana", "laranja"]
+for frutas in frutas:
+    print("Eu gosto de {frutas}")
+
+aluno = ["Ana Júlia", "Alexandre", "Asafe", "Brayan"]
+numero_chamada = 1
+
+for aluno in aluno:
+    print(f"{numero_chamada} -- {aluno}")
+    numero_chamada = 1
+
+def soma():
+    numeroA = 2
+    numeroB = 4
+    print("Vamos somar dois números: ")
+    resultado = numeroA + numeroB
+
+soma()
